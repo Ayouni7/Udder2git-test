630504c35decf4eddc40bb2c90fc33175874917d
Mohamad Alayouni <Ayounisvision@gmail.com>
2024-11-01 08:22:52 -0700

    Initial Commit

---
+++ Udder2git test/Udder2git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udder2git_testApp.swift
+//  Udder2git test
+//
+//  Created by Mohamad Alayouni on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Udder2git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Udder2git testTests/Udder2git_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  Udder2git_testTests.swift
+//  Udder2git testTests
+//
+//  Created by Mohamad Alayouni on 11/1/24.
+//
+
+import Testing
+@testable import Udder2git_test
+
+struct Udder2git_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
